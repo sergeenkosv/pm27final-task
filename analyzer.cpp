ae4485706e72fd6d274699be39a26a3df13550e0
С.В. Сергеенко <sserg.me@gmail.com>
2015-03-13 12:28:24 +0300

    Общая структура решения
    Добавлен вывод результата, но подсчет результата представлен пустыми
    функциями возведения в целую стьепень и извлечения целого корня

--- analyzer.cpp
+++ analyzer.cpp
@@ -4,10 +4,19 @@ using namespace std;
 
 /* Вычисление расиональной степени числа */
 
+double power(double x, int n)
+{
+}
+
+double root(double x, int n)
+{
+}
+
 int main()
 {
 	int n, m;
 	double q;
 	cin >> q >> n >> m;
 	/* q ^ (n/m) */
+	cout << root(power(q, n), m) << endl;
 }
